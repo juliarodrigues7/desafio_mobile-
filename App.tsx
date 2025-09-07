010e622f54589e29cf21e100f961b11ae120aedb
julialima7 <julia.lrodrigues7@gmail.com>
2025-09-07 19:49:53 -0300

    Import quotes Json and create motivational and religious arrays

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import quotesData from "./data/quotes.json";
 
 export default function App() {
   return (
@@ -10,11 +11,18 @@ export default function App() {
   );
 }
 
+const motivationalQuotes = quotesData.quotes.filter(
+  (q) => q.category !== "religious"
+);
+const religiousQuotes = quotesData.quotes.filter(
+  (q) => q.category === "religious"
+);
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
